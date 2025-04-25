706f6bc55a9363b59582e2ec3aa8f80982c1fa05
zhangzexin <zzx-nuc@qq.com>
2025-04-25 17:10:14 +0800

    调整样式

--- src/views/deepseek/index.vue
+++ src/views/deepseek/index.vue
@@ -250,12 +250,13 @@ const handleRequest = async () => {
     
       const reader = response.body.getReader();
       const decoder = new TextDecoder();
-      
+
       while (true) {
         const { done, value } = await reader.read();
         if (done) break;
         
         const chunk = decoder.decode(value, { stream: true });
+        console.log("chunk----", chunk);
         const data = JSON.parse(chunk);
         
         if (data?.candidates?.[0]?.content?.parts?.[0]?.text) {
